b556426b690d5bcc2a16fd398435c89bb14b44c7
prajapati-priyanka <81961482+prajapati-priyanka@users.noreply.github.com>
2021-09-01 01:14:02 +0530

    fix:  add Number function to inputs
    added Number function to inputs to convert .values from string to int

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const tableCaption = document.querySelector("#table-caption");
 const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
 
 function nextButtonHandler() {
-  if (billAmount.value > 0) {
+  if (Number(billAmount.value) > 0) {
     cashGivenInput.style.display = "block";
     btnNext.style.display = "none";
     errorMessage.style.display = "none";
@@ -21,24 +21,24 @@ function nextButtonHandler() {
 }
 
 function calculateAmountToBeReturned() {
-  if (cashAmount.value > 0) {
-    if (cashAmount.value === billAmount.value) {
+  if (Number(cashAmount.value) > 0) {
+    if (Number(cashAmount.value) === Number(billAmount.value)) {
       errorMessage.style.display = "block";
       tableChange.style.display = "none";
       tableCaption.style.display = "none";
       errorMessage.innerText = "No Change to return";
-    } else if (cashAmount.value < billAmount.value) {
+    } else if (Number(cashAmount.value) < Number(billAmount.value)) {
       errorMessage.style.display = "block";
       tableChange.style.display = "none";
       tableCaption.style.display = "none";
       errorMessage.innerText =
         "Cash is less than bill, please enter right amount";
-    } else if (cashAmount.value > billAmount.value) {
+    } else if (Number(cashAmount.value)> Number(billAmount.value)) {
       errorMessage.style.display = "none";
       tableChange.style.display = "block";
       tableCaption.style.display = "block";
 
-      let amountToBeReturned = cashAmount.value - billAmount.value;
+      let amountToBeReturned = Number(cashAmount.value) -Number(billAmount.value);
 
       for (let i = 0; i < availableNotes.length; i++) {
         let numberOfNotes = Math.trunc(amountToBeReturned / availableNotes[i]);
